5b064919b9a7ca81243677a04f04c107f8290709
Cihan Emre Kisakurek (Company) <cihan@cihan-air.bln.int.planetromeo.com>
2016-02-08 14:19:45 +0100

    Initial Commit

---
+++ Modar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Modar
+//
+//  Created by Cihan Emre Kisakurek (Company) on 08/02/16.
+//  Copyright (c) 2016 cekisakurek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Modar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Modar
+//
+//  Created by Cihan Emre Kisakurek (Company) on 08/02/16.
+//  Copyright (c) 2016 cekisakurek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
